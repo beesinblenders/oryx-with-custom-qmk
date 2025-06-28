55b05d744f3fb522f7f1fa384cd70263b42c647d
josephtribulat <128245994+josephtribulat@users.noreply.github.com>
2025-06-28 18:44:14 +0200

    oui

--- GqBp3/config.h
+++ GqBp3/config.h
@@ -14,9 +14,3 @@
 #define HSS(report) host_system_send(record->event.pressed ? report : 0); return false
 #define HCS(report) host_consumer_send(record->event.pressed ? report : 0); return false
 #define TAPPING_TERM_PER_KEY
-
-
-
-
-
-#define PERMISSIVE_HOLD
